469aa1aea2218cba2dca0ab3a339a895bb6cd3b6
sonalkumari-2000 <161581076+sonalkumari-2000@users.noreply.github.com>
2024-03-05 21:33:05 +0530

    Add files via upload

---
+++ length .cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+	stack<int>a,b;
+	a.push(8),a.push(12),a.push(56);
+	b.push(23),b.push(90),b.push(3),b.push(66);
+		cout<<"size of a:"<<a.size();
+		cout<<"size of b:"<<b.size();
+	}
+
+
+
