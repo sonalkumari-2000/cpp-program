c8ff4af4f8ccc5f7af21daf340e0ebfabeb6ea87
sonalkumari-2000 <sonal2000kumari@gmail.com>
2024-06-11 14:43:03 +0530

    size change

--- length .cpp
+++ length .cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
 	stack<int>a,b;
-	a.push(8),a.push(12),a.push(56);
+	a.push(8),a.push(12),a.push(56),a.push(70);
 	b.push(23),b.push(90),b.push(3),b.push(66);
 		cout<<"size of a:"<<a.size();
 		cout<<"size of b:"<<b.size();
